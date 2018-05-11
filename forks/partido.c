e96661772cc750077c998f5a0e0dd40833ecf6cd
Cesar Peralta <cperaltcoto@gmail.com>
2018-05-11 15:58:31 -0600

    teams implemented

--- forks/partido.c
+++ forks/partido.c
@@ -10,6 +10,8 @@
 
 #include <unistd.h>
 
+#include <signal.h>
+
 
 
 #include "semun.h"
@@ -24,7 +26,8 @@ static int semaphore_p(int sem_num);
 
 static int semaphore_v(int sem_num);
 
-
+static void playerTeamA(int goalB);
+static void playerTeamB(int goalA);
 
 
 
@@ -32,22 +35,21 @@ static int sem_id;
 
 
 
-int main(int argc, char *argv[]) {
+int main(void) {
 
 
 
 	int i;
+	
+	int n = 4; // Total players
 
 	int pause_time;
 
-	//char op_char = 'O';
+	int goalA;
 
-	int goalA = 0;
-
-	int goalB = 0;
+	int goalB;
 
 	printf("Start Match\n");
-
 
 
 	srand((unsigned int)getpid());
@@ -56,7 +58,7 @@ int main(int argc, char *argv[]) {
 
 
 
-	if(argc > 1)
+	if(1)
 
 	{
 
@@ -68,104 +70,228 @@ int main(int argc, char *argv[]) {
 
 		}
 
-		//op_char = 'X';
-
+		goalA = 0;
+		goalB = 0;
+		printf("Semaphores created\n");
 		sleep(2);
 
 	}
 
 
-	pid_t pid;
-
-
-
 	/*child*/
 
 	/*Team 1*/
-
-	pid = fork();
-	pid = fork();
-	pid = fork();
-
-
-
-
-	if (pid < 0) /*error*/
-
+	
+	pid_t pids[10];
+	
+	for(i = 0; i < n; i++)
 	{
-
-		fprintf(stderr, "fork failed");
-
-		return 1;
-
+		if((pids[i] = fork()) < 0)
+		{
+			perror("fork error");
+			abort();
+		}
+		else if (pids[i] == 0) 
+		{
+			
+			if(i % 2 == 0)
+			{
+				printf("Player ID: %d created; team B\n",getpid() );
+				playerTeamB(goalA);
+			}
+			else
+			{
+				printf("Player ID: %d created; team A\n",getpid() );
+				playerTeamA(goalB);
+			}
+			exit(0);
+		}
 	}
 
-	else if (pid == 0) /*child process*/
-
+	sleep(300);
+	for(i = 0; i < 10; i++)
 	{
+		kill(pids[i], SIGKILL);
+		printf("process %d terminated\n", pids[i]);	
+	}
+	
+	
+	printf("\n%d - match finished\n", getpid());
 
-		printf("process created PID: %d\n",getpid() );
+	del_semvalue();
+	
+	printf("\nFinal Score: Team A %d  --  %d Team B\n", goalB, goalA);
+
+	exit(EXIT_SUCCESS);
+
+
+	return 0;
+
+}
+
+
+static void playerTeamA(int goalB)
+{
+	int i;
+
+	int pause_timeA;
+
+
+	while(1)
+	{
 
 		/*Wait time*/
 
-		pause_time = ((rand() % 15) + 5);
+		pause_timeA = ((rand() % 15) + 5);
+		printf("Pause time A: %d\n", pause_timeA );
+		sleep(pause_timeA);
 
-		sleep(pause_time);
-
-		printf("trying to get the ball PID: %d\n",getpid() );
+		printf("PID: %d trying to get the ball\n",getpid() );
 
 		if (!semaphore_p(0))
 
 		{
-			printf("unable to get the ball PID: %d\n",getpid() );
+			printf("PID: %d unable to get the ball\n",getpid() );
+			exit(EXIT_FAILURE);
+
+		}
+		else
+		{
+
+			printf("PID: %d got the ball\n", getpid());
+
+			for (i = 0; i < 3; ++i)
+			{
+				printf("PID: %d trying to get the goal\n",getpid() );
+
+
+				if (!semaphore_p(2))
+
+				{
+					//pause_time = rand() % 1000;
+
+					sleep(1);
+					printf("PID: %d unable to get the goal\n",getpid() );
+
+					exit(EXIT_FAILURE);
+
+				}
+
+				else
+
+				{
+
+					printf("GOOOOOLLLLL of PID: %d\n",getpid() );
+					goalB += 1;
+					printf("Team A scores!!!! %d goals\n", goalB);
+					i = 3;
+				}
+
+
+				if(!semaphore_v(2))
+				{
+					exit(EXIT_FAILURE);
+				}
+
+			}
+		}
+
+		if(!semaphore_v(0))
+		{
+			exit(EXIT_FAILURE);
+		}
+
+			
+		if (!semaphore_p(1))
+		{
 
 			exit(EXIT_FAILURE);
 
 		}
 
-		printf("PID1: %d - got the ball\n", getpid());
-
-			//pause_time = rand() % 1000;
-
-			//sleep(pause_time);
-
-			////printf("%c\n", op_char);
-
-		for (i = 0; i < 3; ++i)
+		else
 
 		{
-			printf("trying to get the goal PID: %d\n",getpid() );
 
-			if (!semaphore_p(2))
+			printf("PID: %d is defending\n",getpid() );
 
-			{
-
-				//pause_time = rand() % 1000;
-
-				sleep(1);
-
-				exit(EXIT_FAILURE);
-
-			}
-
-			else
-
-			{
-
-				printf("GOOOOOLLLLL PID: %d\n",getpid() );
-				goalB += 1;
-
-			}
-
-			if(!semaphore_v(2)){
-
-				exit(EXIT_FAILURE);
-
-			}
+			pause_timeA = rand() % 2;
+			printf("Pause time A: %d\n", pause_timeA );
+			sleep(2);
 
 		}
 
+		if(!semaphore_v(1)){
 
+			exit(EXIT_FAILURE);
+		}
+	}
+}
+
+
+static void playerTeamB(int goalA)
+{	
+
+	int i;
+
+	int pause_timeB;
+
+	while(1)
+	{
+
+	/*Wait time*/
+
+		pause_timeB = ((rand() % 15) + 5);
+		printf("Pause time B: %d\n", pause_timeB );
+		sleep(pause_timeB);
+
+		printf("PID: %d trying to get the ball\n",getpid() );
+
+		if (!semaphore_p(0))
+		{
+			printf("PID: %d unable to get the ball\n",getpid() );
+			exit(EXIT_FAILURE);
+
+		}
+		else
+		{
+
+			printf("PID: %d got the ball\n", getpid());
+
+			for (i = 0; i < 3; ++i)
+			{
+				printf("PID: %d trying to get the goal\n",getpid() );
+
+
+				if (!semaphore_p(1))
+
+				{
+					//pause_time = rand() % 1000;
+
+					sleep(1);
+					printf("PID: %d unable to get the goal\n",getpid() );
+
+					exit(EXIT_FAILURE);
+
+				}
+
+				else
+
+				{
+					printf("GOOOOOLLLLL of PID: %d\n",getpid() );
+					goalA += 1;
+					printf("Team B scores!!!! %d goals\n", goalA);
+					i = 3;
+				}
+
+
+				if(!semaphore_v(1)){
+
+					exit(EXIT_FAILURE);
+				}
+
+			}
+		}
 
 		if(!semaphore_v(0)){
 
@@ -173,84 +299,32 @@ int main(int argc, char *argv[]) {
 
 		}
 
-		
-
-		if (!semaphore_p(1))
-
-			{
-
-				exit(EXIT_FAILURE);
-
-			}
-
-			else
-
-			{
-
-				printf("PID: %d is defending\n",getpid() );
-
-				pause_time = rand() % 2;
-
-				sleep(pause_time);
-
-			}
-
-			if(!semaphore_v(1)){
-
-				exit(EXIT_FAILURE);
-
-			}
-
-
-
-/*		printf("\n%d - finished\n", getpid());
-
-		if (argc > 1)
+		if (!semaphore_p(2))
 
 		{
 
-			pause_time = rand() % 15 + 5;
-
-			sleep(pause_time);
-
-			del_semvalue();
+			exit(EXIT_FAILURE);
 
 		}
 
-		exit(EXIT_SUCCESS);*/
-
-	}
-
-	else /*parent process*/
-
-	{
-
-		/*print score*/
-
-		wait(NULL);
-
-		printf("Child complete");
-
-		printf("\n%d - match finished\n", getpid());
-
-		if (argc > 1)
+		else
 
 		{
 
-			del_semvalue();
+			printf("PID: %d is defending\n",getpid() );
+
+			pause_timeB = rand() % 2;
+			printf("Pause time B: %d\n", pause_timeB );
+			sleep(2);
 
 		}
 
-		exit(EXIT_SUCCESS);
+		if(!semaphore_v(2)){
 
+			exit(EXIT_FAILURE);
+		}
 	}
-
-
-
-	return 0;
-
 }
-
 
 
 static void del_semvalue(void)
@@ -264,7 +338,6 @@ static void del_semvalue(void)
 		fprintf(stderr, "Failed to delete semaphore\n");
 
 	}
-
 }
 
 static int set_semvalue(void)
@@ -274,6 +347,16 @@ static int set_semvalue(void)
 	sem_union.val = 1;
 	
 	if(semctl(sem_id,0,SETVAL,sem_union) == -1){
+
+		return(0);
+
+	}
+	if(semctl(sem_id,1,SETVAL,sem_union) == -1){
+
+		return(0);
+
+	}
+	if(semctl(sem_id,2,SETVAL,sem_union) == -1){
 
 		return(0);
 
@@ -300,8 +383,6 @@ static int semaphore_p(int sem_num)
 {
 
 	struct sembuf sem_b;
-
-
 
 	sem_b.sem_num = sem_num;
 
